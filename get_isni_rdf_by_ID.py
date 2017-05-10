bfca230ac311d8e1f2607b5c70b5e2c839d3befd
eslao <christine.eslao@gmail.com>
2017-05-10 15:58:18 -0400

    Add error handling for blank RDF files

--- get_isni_rdf_by_ID.py
+++ get_isni_rdf_by_ID.py
@@ -1,17 +1,27 @@
-identifiers = ['0000 0000 7328 0710', '0000 0000 6648 4467', '0000 0000 4977 6257']
+identifiers = ['0000 0000 7328 0710', '0000 0001 2103 7046', '0000 0000 6648 4467', '0000 0000 4977 6257']
 uris = []
 for isni_number in identifiers:
     uris += ['http://www.isni.org/isni/' + isni_number.replace(' ', '')]
-print uris
+#print uris
+
+rdf_urls = []
+
+for uri in uris:
+    rdf_content = requests.get(uri + '.rdf')
+    if rdf_content.text != '':
+        rdf_urls += [uri + '.rdf']
+    else:
+        print uri + ' is a problem'
+        
+#print rdf_urls
 
 g2 = rdflib.Graph()
 
-for uri in uris:
-    print uri
+for rdf_url in rdf_urls:
+    #print rdf_url
     g1 = rdflib.Graph()
-    rdfurl = uri + '.rdf'
-    g1.parse(rdfurl, format='xml')
+    g1.parse(rdf_url, format='xml')
     for s, p, o in g1:
-        g2.add( (URIRef(uri), p, o) )
+        g2.add( (URIRef(rdf_url), p, o) )
         
 print( g2.serialize(format='n3') )
